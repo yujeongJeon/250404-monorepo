e8a8b79a4e9f41e3918c4f5334ec714db3f81015
yujeongJeon <workingnewjeong@gmail.com>
2025-04-03 23:21:26 +0900

    🔧 Install eslint, prettier and set configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import naverpay from '@naverpay/eslint-config'
+import {defineConfig} from 'eslint/config'
+
+export default defineConfig([
+    {
+        ignores: ['.changeset/*'],
+    },
+    ...naverpay.configs.node,
+    ...naverpay.configs.packageJson,
+    {
+        rules: {
+            'no-console': 'off',
+        },
+    },
+])
